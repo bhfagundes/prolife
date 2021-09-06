6078a1ed53f59ed6cfe86504910f0a936ea300b4
Brenner Fagundes <bhfagundes@gmail.com>
2021-09-06 17:19:21 -0300

    ajustando nome de coluna de contatos

--- database/migrations/2021_09_06_165400_create_contact_table.php
+++ database/migrations/2021_09_06_165400_create_contact_table.php
@@ -18,7 +18,7 @@ class CreateContactTable extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('phone');
-            $table->string('mensagem');
+            $table->string('message');
             $table->string('file');
             $table->timestamps();
         });
